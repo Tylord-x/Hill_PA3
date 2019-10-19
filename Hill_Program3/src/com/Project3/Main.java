5f0d63a82b706542dae80493bf23126f88628165
Tyler <tylerhill420@gmail.com>
2019-10-19 12:13:45 -0400

    Part B

--- Hill_Program3/src/com/Project3/Main.java
+++ Hill_Program3/src/com/Project3/Main.java
@@ -4,29 +4,63 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner scnr = new Scanner(System.in);
-    static int multiplication(int n1, int n2) {
-        int m = n1 * n2;
-        while (true) {
-            System.out.println("How much is " + n1 + " times " + n2 + "?");
-            int n3 = scnr.nextInt();
-            if (n3 == m6){
-                System.out.println("Very Good");
+    static Random SecureRandom = new Random();
+    static void correct() {
+        int correct = SecureRandom.nextInt(4);
+        switch (correct) {
+            case 0:
+                System.out.println("Very Good !");
                 break;
-            } else {
-                System.out.println("No, Please try again");
+            case 1:
+                System.out.println("Excellent !");
+                break;
+            case 2:
+                System.out.println("Nice Work !");
+                break;
+            case 3:
+                System.out.println("Keep up the good work !");
+                break;
+        }
+    }
+    static void incorrect() {
+        int incorrect = SecureRandom.nextInt(4);
+        switch (incorrect) {
+            case 0:
+                System.out.println("Very Good !");
+                break;
+            case 1:
+                System.out.println("Excellent !");
+                break;
+            case 2:
+                System.out.println("Nice Work !");
+                break;
+            case 3:
+                System.out.println("Keep up the good work !");
+                break;
+        }
+    }
+        static Scanner scnr = new Scanner(System.in);
+        static int multiplication ( int n1, int n2){
+            int m = n1 * n2;
+            while (true) {
+                System.out.println("How much is " + n1 + " times " + n2 + "?");
+                int n3 = scnr.nextInt();
+                if (n3 == m) {
+                    System.out.println("Very Good");
+                    break;
+                } else {
+                    System.out.println("No, Please try again");
+                }
+            }
+            return 0;
+        }
+
+        public static void main (String[]args){
+            int n1, n2;
+            while (true) {
+                n1 = SecureRandom.nextInt(10);
+                n2 = SecureRandom.nextInt(10);
+                multiplication(n1, n2);
             }
         }
-        return 0;
     }
-
-    public static void main(String[] args) {
-        int n1, n2;
-        while (true) {
-            Random SecureRandom = new Random();
-            n1 = SecureRandom.nextInt(10);
-            n2 = SecureRandom.nextInt(10);
-            multiplication(n1, n2);
-        }
-    }
-}
